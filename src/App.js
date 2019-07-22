b9469a8bef73afa62dae5e9120102d59174408b8
carlos augusto <carlos.silva@processmind.com.br>
2019-07-22 17:03:36 -0300

    criação de página de login

--- src/App.js
+++ src/App.js
@@ -9,7 +9,34 @@ function App() {
       <Header />
 
       <main className="main d-inline-flex w-100 justify-content-center align-items-center">
-        <div className="main_content">Teste</div>
+        <form action="GET" autoComplete="off" className="formContent row" noValidate>
+          <div className="col-12">
+            <div className="form-group">
+              <label className="" htmlFor="email">E-mail</label>
+              <input className="form-control form-control-sm" type="text" id="email"
+                     placeholder="Ex: jose.teste@gmail.com" />
+            </div>
+          </div>
+
+          <div className="col-12">
+            <div className="form-group">
+              <label className="" htmlFor="password">Senha</label>
+              <input className="form-control form-control-sm" type="password" id="password" placeholder="* * * * * *" />
+            </div>
+          </div>
+
+          <div className="col-12">
+            <div className="row no-gutters justify-content-end">
+              <div className="d-inline-block">
+                <a className="btn btn-sm btn-link">Cadastrar</a>
+              </div>
+
+              <div className="d-inline-block ml-1">
+                <button className="btn btn-sm btn-primary" type="submit">Entrar</button>
+              </div>
+            </div>
+          </div>
+        </form>
       </main>
     </div>
   );
