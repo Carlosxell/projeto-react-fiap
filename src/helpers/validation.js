f2386932cc18a7588ffb2086d5b4fcc0f0aca037
carlos augusto <carlos.silva@processmind.com.br>
2019-07-25 17:02:06 -0300

    confirmação de validação para página de cadastro de usuários[JS]

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,6 +1,9 @@
 let yup = require('yup');
 
 export const validationSchema = yup.object().shape({
-  email: yup.string().email('E-mail não é válido!').required('E-mail é um Campo Obrigatório!'),
-  password: yup.string().min(6, 'A senha deve ter ao menos 6 caracteres!').required('Campo Senha é Obrigatório!')
+  cpf: yup.string().required('CPF é um campo obrigatório!').min(14, `O campo CPF deve ter 14 caracteres com números, '.' e '-'.`),
+  name: yup.string().required('Nome é um campo obrigatório!'),
+  email: yup.string().email('E-mail não é válido!').required('E-mail é um campo obrigatório!'),
+  password: yup.string().min(6, 'A senha deve ter ao menos 6 caracteres!').required('campo senha é obrigatório!'),
+  checkPassword: yup.string().min(6, 'A confirmação de senha deve ter ao menos 6 caracteres!').required('A confirmação de senha é obrigatório!'),
 });
--- src/views/CadastroCliente.js
+++ src/views/CadastroCliente.js
@@ -4,8 +4,10 @@ import { validationSchema } from '../helpers/validation';
 import { Link } from "react-router-dom";
 
 const initState = {
+  cpf: "",
+  name: "",
   email: "",
-  password: ""
+  password: "",
 };
 
 function CadastroCliente(props) {
@@ -13,65 +15,101 @@ function CadastroCliente(props) {
 
   return (
     <Formik initialValues={ initState } onSubmit={ onSubmit } validationSchema={ validationSchema }>
-      <form action="GET" autoComplete="off" className="formContent" noValidate>
-        <div className="row">
-          <div className="col-12">
-            <div className="form-group">
-              <label className="" htmlFor="nome">Nome</label>
-              <input className="form-control form-control-sm" type="text" id="nome" placeholder="Ex: José da Silva"/>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-4">
-            <div className="form-group">
-              <label className="" htmlFor="cpf">CPF</label>
-              <input className="form-control form-control-sm" type="text" id="cpf" placeholder="000.000.000-00"/>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-4">
-            <div className="form-group">
-              <label className="" htmlFor="telefone">Telefone</label>
-              <input className="form-control form-control-sm" type="text" id="telefone" placeholder="(00) 00000-0000"/>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-4">
-            <div className="form-group">
-              <label className="" htmlFor="email">E-mail</label>
-              <input className="form-control form-control-sm" type="text" id="email"
-                     placeholder="Ex: jose.teste@gmail.com"/>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6">
-            <div className="form-group">
-              <label className="" htmlFor="password">Senha</label>
-              <input className="form-control form-control-sm" type="password" id="password" placeholder="* * * * * *"/>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6">
-            <div className="form-group">
-              <label className="" htmlFor="confirmePassword">Confirme a Senha</label>
-              <input className="form-control form-control-sm" type="password" id="confirmePassword"
-                     placeholder="* * * * * *"/>
-            </div>
-          </div>
-
-          <div className="col-12">
-            <div className="row no-gutters justify-content-end">
-              <div className="d-inline-block">
-                <Link className="btn btn-sm btn-outline-secondary mr-1" to={ '/' }>Voltar</Link>
-              </div>
-
-              <div className="d-inline-block">
-                <a className="btn btn-sm btn-primary" href="../views/dashboard.html">Enviar Cadastro</a>
+      { props => (
+        <form action="GET" autoComplete="off" className="formContent" noValidate>
+          <div className="row">
+            <div className="col-12">
+              <div className="form-group">
+                <label className="" htmlFor="name">Nome</label>
+                <Field onChange={ props.handleChange }
+                       className={`form-control form-control-sm ${ props.errors.name ? 'is-invalid' : ''}`}
+                       id="name"
+                       name="name"
+                       placeholder="Ex: José da Silva"
+                       type="text"
+                       value={ props.values.name } />
+                <small className="invalid-feedback">{ props.errors.name }</small>
               </div>
             </div>
+
+            <div className="col-12 col-sm-4">
+              <div className="form-group">
+                <label className="" htmlFor="cpf">CPF</label>
+                <Field onChange={ props.handleChange }
+                       className={`form-control form-control-sm ${ props.errors.cpf ? 'is-invalid' : ''}`}
+                       id="cpf"
+                       maxLength="14"
+                       name="cpf"
+                       placeholder="000.000.000-00"
+                       type="text"
+                       value={ props.values.cpf } />
+                <small className="invalid-feedback">{ props.errors.cpf }</small>
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-4">
+              <div className="form-group">
+                <label className="" htmlFor="telefone">Telefone</label>
+                <input className="form-control form-control-sm" type="text" id="telefone" placeholder="(00) 00000-0000"/>
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-4">
+              <div className="form-group">
+                <label className="" htmlFor="email">E-mail</label>
+                <Field onChange={ props.handleChange }
+                       className={`form-control form-control-sm ${ props.errors.email ? 'is-invalid' : ''}`}
+                       id="email"
+                       name="email"
+                       placeholder="Ex: jose.teste@gmail.com"
+                       type="text"
+                       value={ props.values.email } />
+                <small className="invalid-feedback">{ props.errors.email }</small>
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-6">
+              <div className="form-group">
+                <label className="" htmlFor="password">Senha</label>
+                <Field onChange={ props.handleChange }
+                       className={`form-control form-control-sm ${ props.errors.password ? 'is-invalid' : ''}`}
+                       id="password"
+                       name="password"
+                       placeholder="* * * * * *"
+                       type="password"
+                       value={ props.values.password } />
+                <small className="invalid-feedback">{ props.errors.password }</small>
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-6">
+              <div className="form-group">
+                <label className="" htmlFor="checkPassword">Confirme a Senha</label>
+                <Field onChange={ props.handleChange }
+                       className={`form-control form-control-sm ${ props.errors.checkPassword ? 'is-invalid' : ''}`}
+                       id="checkPassword"
+                       name="checkPassword"
+                       placeholder="* * * * * *"
+                       type="password"
+                       value={ props.values.checkPassword } />
+                <small className="invalid-feedback">{ props.errors.checkPassword }</small>
+              </div>
+            </div>
+
+            <div className="col-12">
+              <div className="row no-gutters justify-content-end">
+                <div className="d-inline-block">
+                  <Link className="btn btn-sm btn-link mr-1" to={ '/' }>Voltar</Link>
+                </div>
+
+                <div className="d-inline-block">
+                  <a className="btn btn-sm btn-primary" href="../views/dashboard.html">Enviar Cadastro</a>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      ) }
     </Formik>
   );
 }
